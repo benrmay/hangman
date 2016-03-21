5bc9629eacd7f5ddcd167fec04add7f4ff97e930
Benjamin May <benrmay1@gmail.com>
2016-03-21 11:31:30 -0700

    Added word bank, and improved print output

--- hangman.py
+++ hangman.py
@@ -3,8 +3,7 @@ game: Hangman -- a python project
 author: benrmay
 
 """
-guessWord = 'benrmaygame'
-knownWord = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
+guessWordList = 'laser diagonal comfy myth dorsal biscuit hydrogen macaroni rubber darkness yolk exercise vegetarian shrew chestnut ditch wobble glitter neighborhood dizzy fire side retail drawback logo fabric mirror barber jazz migrate drought commercial dashboard bargain double download professor landscape ski goggles vitamin'.split()
 hangmanGraphic = ['''
 ________      
 |      |
@@ -12,6 +11,7 @@ ________
 |
 |
 |
+========
 ''',
                   '''
 ________
@@ -20,6 +20,7 @@ ________
 |
 |
 |
+========
 ''',
                   '''
 ________
@@ -28,24 +29,30 @@ ________
 |      |
 |
 |
+========
 ''',
                   '''
 ________
 |      |
 |      O
 |      |
-|     / \
+|     / \\
 |
+========
 ''',
                   '''
 ________
 |      |
 |     _O_
 |      |
-|     / \
+|     / \\
 |
+========
 ''']
 
+import hangmanHelper #this file has the hangman pictures and the guess word list.
+import random
+    
 def startGame():
     print("Welcome to Hangman, a game created by benrmay.")
     startInput = input("Would you like to start the game? (y/n)")
@@ -53,14 +60,16 @@ def startGame():
         print ("Well then, goodbye. See you next Wednesday.")
         return
     else:
+        guessWord = getGuessWord(guessWordList)
+        knownWord = createKnownWord(guessWord)
         numGuesses = 0
         guessedCharList = []
         while numGuesses < 5:
             print(hangmanGraphic[numGuesses])
-            print(knownWord)
+            printKnownWord(knownWord)
             letter = input("Guess a letter: ")
             i = 0
-            check = alreadyGuessedLetter(letter)
+            check = alreadyGuessedLetter(letter, knownWord)
             if check == False:
                 guessFlag = False
                 for char in guessWord:
@@ -75,20 +84,37 @@ def startGame():
                 elif guessFlag == True:
                     print("Congrats! You got at least a letter")
                     guessedCharList.append(letter)
-            if isGameWon() == True:
+            if isGameWon(knownWord) == True:
                 print("Congrats! You won the game!!!")
                 print("The word was " + guessWord + ".")
                 return
+        print("You lost! Try again some other time.")
+        print("The word was " + guessWord + ".")
+        return
     
-def alreadyGuessedLetter(letter):
+def alreadyGuessedLetter(letter, knownWord):
     for char in knownWord:
         if letter == char:
             print("You already tried " + letter + ". Try again!")
             return True
     return False
 
-def isGameWon():
+def isGameWon(knownWord):
     for char in knownWord:
         if char == '_':
             return False
     return True
+
+def getGuessWord(guessWordList):
+    return guessWordList[random.randint(0, len(guessWordList) - 1)]
+
+def createKnownWord(guessWord):
+    knownWord = []
+    for char in guessWord:
+        knownWord.append('_')
+    return knownWord
+
+def printKnownWord(knownWord):
+    tempStr = ''.join(knownWord)
+    print(" ".join(tempStr))
+    return
